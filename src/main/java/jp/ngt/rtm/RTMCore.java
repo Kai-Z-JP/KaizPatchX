5ab24f9b09d4349916fbb493977d5aa0debdc5c5
Kaiz_JP <kaiz@kaiz.jp>
2020-11-03 16:32:09 +0900

    KaizPatch13

--- src/main/java/jp/ngt/rtm/RTMCore.java
+++ src/main/java/jp/ngt/rtm/RTMCore.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Level;
 @Mod(modid = RTMCore.MODID, name = "RealTrainMod", version = RTMCore.VERSION)
 public final class RTMCore {
 	public static final String MODID = "RTM";
-	public static final String VERSION = "1.7.10.41_KaizPatch12";
+	public static final String VERSION = "1.7.10.41_KaizPatch13";
 
 	@Instance(MODID)
 	public static RTMCore instance;

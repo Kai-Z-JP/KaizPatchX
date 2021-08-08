4f08e5d7c0fadf446d96093424a589f47f0b876a
Kaiz <kaiz@kaiz.jp>
2021-08-08 15:35:54 +0900

    [Update] 1.3.0-RC2

--- src/main/java/jp/ngt/rtm/RTMCore.java
+++ src/main/java/jp/ngt/rtm/RTMCore.java
@@ -26,7 +26,7 @@ import paulscode.sound.SoundSystemConfig;
 @Mod(modid = RTMCore.MODID, name = "RealTrainMod", version = RTMCore.VERSION)
 public final class RTMCore {
     public static final String MODID = "RTM";
-    public static final String VERSION = "1.7.10.41_KaizPatchX1.3RC1";
+    public static final String VERSION = "1.7.10.41_KaizPatchX1.3.0-RC2";
 
     @Instance(MODID)
     public static RTMCore instance;

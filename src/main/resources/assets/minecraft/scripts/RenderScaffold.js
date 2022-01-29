f9cae702b3fb187475515deaa950a277543cfb28
Kaiz <kaiz@kaiz.jp>
2022-01-30 04:30:20 +0900

    [Fix] Rendering position of scaffolding railings

--- src/main/resources/assets/minecraft/scripts/RenderScaffold.js
+++ src/main/resources/assets/minecraft/scripts/RenderScaffold.js
@@ -3,6 +3,7 @@ importPackage(Packages.org.lwjgl.opengl);
 importPackage(Packages.jp.ngt.ngtlib.renderer);
 importPackage(Packages.jp.ngt.rtm.render);
 importPackage(Packages.jp.ngt.rtm.block);
+importPackage(Packages.jp.ngt.rtm.block.tileentity);
 
 function init(par1, par2) {
     partMain = renderer.registerParts(new Parts("partMain"));
@@ -28,32 +29,35 @@ function render(entity, pass, par3) {
             var y = entity.getY();
             var z = entity.getZ();
             var world = renderer.getWorld(entity);
-            var dir = entity.getDir();
-            var b0 = (dir == 0 || dir == 2);
+
+            var b0 = true;
+            if (entity instanceof TileEntityScaffold) {
+                b0 = entity.getDir() === 0;
+            }
 
             //なし:0, 足場Z:1, 足場X:2, 階段:3, 立方体:4
-            var flag0 = BlockScaffold.getConnectionType(world, x + 1, y, z, 1);
-            var flag1 = BlockScaffold.getConnectionType(world, x - 1, y, z, 1);
-            var flag2 = BlockScaffold.getConnectionType(world, x, y, z + 1, 0);
-            var flag3 = BlockScaffold.getConnectionType(world, x, y, z - 1, 0);
-            var flagXP = !(flag0 >= 1 && flag0 <= 3) && (b0 || (flag2 == 1 || flag3 == 1 || flag2 == 3 || flag3 == 3));
-            var flagXN = !(flag1 >= 1 && flag1 <= 3) && (b0 || (flag2 == 1 || flag3 == 1 || flag2 == 3 || flag3 == 3));
-            var flagZP = !(flag2 >= 1 && flag2 <= 3) && (!b0 || (flag0 == 2 || flag1 == 2 || flag0 == 3 || flag1 == 3));
-            var flagZN = !(flag3 >= 1 && flag3 <= 3) && (!b0 || (flag0 == 2 || flag1 == 2 || flag0 == 3 || flag1 == 3));
+            var flag0 = BlockScaffold.getConnectionType(world, x + 1, y, z, 0);
+            var flag1 = BlockScaffold.getConnectionType(world, x - 1, y, z, 0);
+            var flag2 = BlockScaffold.getConnectionType(world, x, y, z + 1, 1);
+            var flag3 = BlockScaffold.getConnectionType(world, x, y, z - 1, 1);
+            var flagXP = (b0 && flag0 == 0) || (!b0 && flag0 == 0 && (flag2 == 1 || flag3 == 1 || flag2 == 3 || flag3 == 3));
+            var flagXN = (b0 && flag1 == 0) || (!b0 && flag1 == 0 && (flag2 == 1 || flag3 == 1 || flag2 == 3 || flag3 == 3));
+            var flagZP = (!b0 && flag2 == 0) || (b0 && flag2 == 0 && (flag0 == 2 || flag1 == 2 || flag0 == 3 || flag1 == 3));
+            var flagZN = (!b0 && flag3 == 0) || (b0 && flag3 == 0 && (flag0 == 2 || flag1 == 2 || flag0 == 3 || flag1 == 3));
 
-            if (flagXP && flag0 != 3) {
+            if (flagXP) {
                 xp.render(renderer);
             }
 
-            if (flagXN && flag1 != 3) {
+            if (flagXN) {
                 xn.render(renderer);
             }
 
-            if (flagZP && flag2 != 3) {
+            if (flagZP) {
                 zp.render(renderer);
             }
 
-            if (flagZN && flag3 != 3) {
+            if (flagZN) {
                 zn.render(renderer);
             }
         } else {

c300332940d8d50176f604da721c3be2fca17702
Kaiz <kaiz@kaiz.jp>
2022-06-12 17:34:00 +0900

    1.5.0-beta.1

--- src/main/java/jp/ngt/rtm/RTMCore.java
+++ src/main/java/jp/ngt/rtm/RTMCore.java
@@ -33,7 +33,7 @@ import java.util.Map;
 @Mod(modid = RTMCore.MODID, name = "RealTrainMod", version = RTMCore.VERSION)
 public final class RTMCore {
     public static final String MODID = "RTM";
-    public static final String VERSION = "1.7.10.41_KaizPatchX1.4.6";
+    public static final String VERSION = "1.7.10.41_KaizPatchX1.5.0-beta.1";
 
     @Instance(MODID)
     public static RTMCore instance;

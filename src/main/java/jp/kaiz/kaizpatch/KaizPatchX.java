6dca8cf8f101d1a6d9fdb393d3a100a71fd8bb36
Kaiz <kaiz@kaiz.jp>
2022-10-02 16:05:22 +0900

    chore(pre-release): publish v1.6.0-rc.1

--- src/main/java/jp/kaiz/kaizpatch/KaizPatchX.java
+++ src/main/java/jp/kaiz/kaizpatch/KaizPatchX.java
@@ -5,6 +5,6 @@ public class KaizPatchX {
     public final static int MAJOR_VERSION = 1;
     public final static int MINOR_VERSION = 6;
     public final static int PATCH_VERSION = 0;
-    public final static String PRE_RELEASE_VERSION = "-beta.1";
+    public final static String PRE_RELEASE_VERSION = "-rc.1";
     public final static String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + PATCH_VERSION + PRE_RELEASE_VERSION;
 }

3b216d26ea25bb4512bfd0dd5788376b220758e9
Kaiz <kaiz@kaiz.jp>
2022-10-23 06:39:23 +0900

    feat(gui): support offset in ticket vendor

--- src/main/java/jp/ngt/rtm/electric/BlockTicketVendor.java
+++ src/main/java/jp/ngt/rtm/electric/BlockTicketVendor.java
@@ -24,6 +24,8 @@ public class BlockTicketVendor extends BlockMachineBase {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
         if (NGTUtil.isEquippedItem(player, RTMItem.crowbar)) {
+            player.openGui(RTMCore.instance, RTMCore.guiIdChangeOffset, player.worldObj, x, y, z);
+            return true;
         }
 
         if (!world.isRemote) {

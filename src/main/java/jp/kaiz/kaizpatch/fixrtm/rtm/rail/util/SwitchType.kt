ba5a19f89752951c77a4032e535cde59a7dd6b84
Kaiz <kaiz@kaiz.jp>
2022-11-03 12:46:26 +0900

    fix: license header
    Co-authored-by: anatawa12 <anatawa12@icloud.com>

--- src/main/java/jp/kaiz/kaizpatch/fixrtm/rtm/rail/util/SwitchType.kt
+++ src/main/java/jp/kaiz/kaizpatch/fixrtm/rtm/rail/util/SwitchType.kt
@@ -1,3 +1,7 @@
+/// Copyright (c) 2022 anatawa12 and other contributors
+/// This file is part of fixRTM, released under GNU LGPL v3 with few exceptions
+/// See LICENSE at https://github.com/fixrtm/fixRTM for more details
+
 package jp.kaiz.kaizpatch.fixrtm.rtm.rail.util
 
 import jp.kaiz.kaizpatch.fixrtm.util.Vec2d

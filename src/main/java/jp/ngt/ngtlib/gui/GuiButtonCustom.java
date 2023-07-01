bb095b2c383af7f2b527479d0da36634890cee19
Kaiz <kaiz@kaiz.jp>
2023-07-01 18:19:36 +0900

    feat: port drawing hover text from mc

--- src/main/java/jp/ngt/ngtlib/gui/GuiButtonCustom.java
+++ src/main/java/jp/ngt/ngtlib/gui/GuiButtonCustom.java
@@ -3,20 +3,27 @@ package jp.ngt.ngtlib.gui;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.RenderHelper;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SideOnly(Side.CLIENT)
 public class GuiButtonCustom extends GuiButton {
-    private final GuiScreen screen;
     private final List<String> tips = new ArrayList<>();
 
-    public GuiButtonCustom(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText, GuiScreen pScr) {
+    public GuiButtonCustom(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText) {
         super(buttonId, x, y, widthIn, heightIn, buttonText);
-        this.screen = pScr;
+    }
+
+    @Deprecated
+    public GuiButtonCustom(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText, GuiScreen pScr) {
+        this(buttonId, x, y, widthIn, heightIn, buttonText);
     }
 
     @Override
@@ -24,7 +31,77 @@ public class GuiButtonCustom extends GuiButton {
         super.drawButton(mc, mouseX, mouseY);
 
         if (this.field_146123_n && !this.tips.isEmpty() && this.visible) {
-            GuiScreenCustom.drawHoveringTextS(this.tips, mouseX, mouseY, this.screen);
+            this.drawHoveringText(this.tips, mouseX, mouseY);
+        }
+    }
+
+    protected void drawHoveringText(List<String> p_146283_1_, int p_146283_2_, int p_146283_3_) {
+        FontRenderer fontRendererObj = Minecraft.getMinecraft().fontRenderer;
+        drawHoveringText(p_146283_1_, p_146283_2_, p_146283_3_, fontRendererObj);
+    }
+
+    protected void drawHoveringText(List<String> p_146283_1_, int p_146283_2_, int p_146283_3_, FontRenderer font) {
+        if (!p_146283_1_.isEmpty()) {
+            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+            RenderHelper.disableStandardItemLighting();
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(GL11.GL_DEPTH_TEST);
+            int k = 0;
+
+            for (String s : p_146283_1_) {
+                int l = font.getStringWidth(s);
+
+                if (l > k) {
+                    k = l;
+                }
+            }
+
+            int j2 = p_146283_2_ + 12;
+            int k2 = p_146283_3_ - 12;
+            int i1 = 8;
+
+            if (p_146283_1_.size() > 1) {
+                i1 += 2 + (p_146283_1_.size() - 1) * 10;
+            }
+
+            if (j2 + k > this.width) {
+                j2 -= 28 + k;
+            }
+
+            if (k2 + i1 + 6 > this.height) {
+                k2 = this.height - i1 - 6;
+            }
+
+            this.zLevel = 300.0F;
+            int j1 = -267386864;
+            this.drawGradientRect(j2 - 3, k2 - 4, j2 + k + 3, k2 - 3, j1, j1);
+            this.drawGradientRect(j2 - 3, k2 + i1 + 3, j2 + k + 3, k2 + i1 + 4, j1, j1);
+            this.drawGradientRect(j2 - 3, k2 - 3, j2 + k + 3, k2 + i1 + 3, j1, j1);
+            this.drawGradientRect(j2 - 4, k2 - 3, j2 - 3, k2 + i1 + 3, j1, j1);
+            this.drawGradientRect(j2 + k + 3, k2 - 3, j2 + k + 4, k2 + i1 + 3, j1, j1);
+            int k1 = 1347420415;
+            int l1 = (k1 & 16711422) >> 1 | k1 & -16777216;
+            this.drawGradientRect(j2 - 3, k2 - 3 + 1, j2 - 3 + 1, k2 + i1 + 3 - 1, k1, l1);
+            this.drawGradientRect(j2 + k + 2, k2 - 3 + 1, j2 + k + 3, k2 + i1 + 3 - 1, k1, l1);
+            this.drawGradientRect(j2 - 3, k2 - 3, j2 + k + 3, k2 - 3 + 1, k1, k1);
+            this.drawGradientRect(j2 - 3, k2 + i1 + 2, j2 + k + 3, k2 + i1 + 3, l1, l1);
+
+            for (int i2 = 0; i2 < p_146283_1_.size(); ++i2) {
+                String s1 = p_146283_1_.get(i2);
+                font.drawStringWithShadow(s1, j2, k2, -1);
+
+                if (i2 == 0) {
+                    k2 += 2;
+                }
+
+                k2 += 10;
+            }
+
+            this.zLevel = 0.0F;
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            RenderHelper.enableStandardItemLighting();
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         }
     }
 

7c13d331f1e9694c684d12da1125f0ba34546d96
Kaiz <kaiz@kaiz.jp>
2025-07-06 19:47:26 +0900

    chore(release): publish v1.9.2

--- src/main/java/jp/kaiz/kaizpatch/KaizPatchX.java
+++ src/main/java/jp/kaiz/kaizpatch/KaizPatchX.java
@@ -3,7 +3,7 @@ package jp.kaiz.kaizpatch;
 public class KaizPatchX {
     public static final int MAJOR_VERSION = 1;
     public static final int MINOR_VERSION = 9;
-    public static final int PATCH_VERSION = 1;
+    public static final int PATCH_VERSION = 2;
     public static final String PRE_RELEASE_VERSION = "";
     public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + PATCH_VERSION + PRE_RELEASE_VERSION;
 }
